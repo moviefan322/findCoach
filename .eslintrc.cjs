bd568621192173055d799e27fd6a36be64fe4f35
moviefan322 <philman202@yahoo.com>
2024-05-18 22:24:04 -0400

    builds out coach list page w base components

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,31 @@
 /* eslint-env node */
 require('@rushstack/eslint-patch/modern-module-resolution')
-
 module.exports = {
   root: true,
-  'extends': [
+  env: {
+    node: true
+  },
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/typescript/recommended',
+    'plugin:prettier/recommended'
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: 2020
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: true,
+        trailingComma: 'none',
+        arrowParens: 'avoid',
+        endOfLine: 'auto'
+      }
+    ]
   }
-}
+},
